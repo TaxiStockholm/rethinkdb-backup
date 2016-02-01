79e9fe8b56cf40c62a352eb6571637addae579e1
Gaurav C <cggaurav@gmail.com>
2016-02-01 15:17:24 +0100

    First version of rethinkdb backup

---
+++ README.md
@@ -0,0 +1,37 @@
+# rethinkdb-backup
+
+This image runs rethinkdb dump to backup data using cronjob to folder `/backup`
+
+## Usage:
+
+    docker run -d \
+        --env RETHINKDB_HOST=rethinkdb.host \
+        --env RETHINKDB_PORT=27017 \
+        --volume host.folder:/backup
+        tutum/rethinkdb-backup
+
+Moreover, if you link `tutum/rethinkdb-backup` to a rethinkdb container(e.g. `tutum/rethinkdb`) with an alias named rethinkdb, this image will try to auto load the `host`, `port` if possible.
+
+    docker run -d -p 27017:27017 -p 28017:28017 -e RETHINKDB_PASS="mypass" --name rethinkdb tutum/rethinkdb
+    docker run -d --link rethinkdb:rethinkdb -v host.folder:/backup tutum/rethinkdb-backup
+
+## Parameters
+
+    RETHINKDB_HOST      the host/ip of your rethinkdb database
+    RETHINKDB_PORT      the port number of your rethinkdb database
+    RETHINKDB_DB        the database name to dump. Default: `--all-databases`
+    EXTRA_OPTS      the extra options to pass to rethinkdb dump command
+    CRON_TIME       the interval of cron job to run rethinkdb dump. `0 0 * * *` by default, which is every day at 00:00
+    MAX_BACKUPS     the number of backups to keep. When reaching the limit, the old backup will be discarded. No limit by default
+    INIT_BACKUP     if set, create a backup when the container starts
+    INIT_RESTORE_LATEST if set, restores latest backup
+
+## Restore from a backup
+
+See the list of backups, you can run:
+
+    docker exec tutum-backup ls /backup
+
+To restore database from a certain backup, simply run:
+
+    docker exec tutum-backup /restore.sh /backup/2015.08.06.171901
---
+++ run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+RETHINKDB_HOST=${RETHINKDB_PORT_28015_TCP_ADDR:-${RETHINKDB_HOST}}
+RETHINKDB_HOST=${RETHINKDB_PORT_1_28015_TCP_ADDR:-${RETHINKDB_HOST}}
+RETHINKDB_PORT=${RETHINKDB_PORT_28015_TCP_PORT:-${RETHINKDB_PORT}}
+RETHINKDB_PORT=${RETHINKDB_PORT_1_28015_TCP_PORT:-${RETHINKDB_PORT}}
+
+[ -z "${RETHINKDB_HOST}" ] && { echo "=> RETHINKDB_HOST cannot be empty" && exit 1; }
+[ -z "${RETHINKDB_PORT}" ] && { echo "=> RETHINKDB_PORT cannot be empty" && exit 1; }
+
+BACKUP_CMD="rethinkdb dump -c ${RETHINKDB_HOST}:${RETHINKDB_PORT} -e ${RETHINKDB_DB} ${EXTRA_OPTS} > /backup/"'${BACKUP_NAME}'
+
+echo "=> Creating backup script"
+rm -f /backup.sh
+cat <<EOF >> /backup.sh
+#!/bin/bash
+MAX_BACKUPS=${MAX_BACKUPS}
+
+BACKUP_NAME=\$(date +\%Y.\%m.\%d.\%H\%M\%S).sql
+
+echo "=> Backup started: \${BACKUP_NAME}"
+if ${BACKUP_CMD} ;then
+    echo "   Backup succeeded"
+else
+    echo "   Backup failed"
+    rm -rf /backup/\${BACKUP_NAME}
+fi
+
+if [ -n "\${MAX_BACKUPS}" ]; then
+    while [ \$(ls /backup -N1 | wc -l) -gt \${MAX_BACKUPS} ];
+    do
+        BACKUP_TO_BE_DELETED=\$(ls /backup -N1 | sort | head -n 1)
+        echo "   Backup \${BACKUP_TO_BE_DELETED} is deleted"
+        rm -rf /backup/\${BACKUP_TO_BE_DELETED}
+    done
+fi
+echo "=> Backup done"
+EOF
+chmod +x /backup.sh
+
+echo "=> Creating restore script"
+rm -f /restore.sh
+cat <<EOF >> /restore.sh
+#!/bin/bash
+echo "=> Restore database from \$1"
+if rethinkdb -c ${RETHINKDB_HOST}:${RETHINKDB_PORT} < \$1 ;then
+    echo "   Restore succeeded"
+else
+    echo "   Restore failed"
+fi
+echo "=> Done"
+EOF
+chmod +x /restore.sh
+
+touch /RETHINKDB_backup.log
+tail -F /RETHINKDB_backup.log &
+
+if [ -n "${INIT_BACKUP}" ]; then
+    echo "=> Create a backup on the startup"
+    /backup.sh
+elif [ -n "${INIT_RESTORE_LATEST}" ]; then
+    echo "=> Restore lates backup"
+    until nc -z $RETHINKDB_HOST $RETHINKDB_PORT
+    do
+        echo "waiting database container..."
+        sleep 1
+    done
+    ls -d -1 /backup/* | tail -1 | xargs /restore.sh
+fi
+
+echo "${CRON_TIME} /backup.sh >> /RETHINKDB_backup.log 2>&1" > /crontab.conf
+crontab  /crontab.conf
+echo "=> Running cron job"
+exec cron -f
