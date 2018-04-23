d798ce7c77ad213d7b2e4a2dae98a9df94e31838
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2018-04-23 18:00:48 +0200

    pass extra_opts to restore also

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "=> Restore database from $1"
-if rethinkdb restore $1 -c ${RETHINKDB_HOST}:${RETHINKDB_PORT} --force ;then
+RESTORE_CMD="rethinkdb restore $1 -c ${RETHINKDB_HOST}:${RETHINKDB_PORT} ${EXTRA_OPTS}"
+echo "=> Restore started: ${RESTORE_CMD}"
+if ${RESTORE_CMD} ;then
     echo "   Restore succeeded"
 else
     echo "   Restore failed"
